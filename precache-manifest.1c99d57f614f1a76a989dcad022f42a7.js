2225a9c4453890a5e757ab2211b4f8e298e6b677
AfrinAj7 <afrin.jamal2001@gmail.com>
2023-02-10 13:23:16 +0530

    Updates

---
+++ precache-manifest.1c99d57f614f1a76a989dcad022f42a7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab8438582d597a497c77fbda0c24a108",
+    "url": "/react-portfolio-master.git/index.html"
+  },
+  {
+    "revision": "6c0a4509ef7cfae19b53",
+    "url": "/react-portfolio-master.git/static/css/2.f00fa05b.chunk.css"
+  },
+  {
+    "revision": "e0c8d434f5084453c2c5",
+    "url": "/react-portfolio-master.git/static/css/main.bb1eb78f.chunk.css"
+  },
+  {
+    "revision": "6c0a4509ef7cfae19b53",
+    "url": "/react-portfolio-master.git/static/js/2.f7fd1c51.chunk.js"
+  },
+  {
+    "revision": "00805778243e590c969fe4f0c68d71f6",
+    "url": "/react-portfolio-master.git/static/js/2.f7fd1c51.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e0c8d434f5084453c2c5",
+    "url": "/react-portfolio-master.git/static/js/main.95ddec91.chunk.js"
+  },
+  {
+    "revision": "19cbb0cd03dcfe00a604",
+    "url": "/react-portfolio-master.git/static/js/runtime-main.063b5d39.js"
+  },
+  {
+    "revision": "ce9b13e58fdd40e48635dd1de6aec90c",
+    "url": "/react-portfolio-master.git/static/media/Hello.ce9b13e5.jpg"
+  },
+  {
+    "revision": "38828042bce3def5dbc3c5b4a5d2df93",
+    "url": "/react-portfolio-master.git/static/media/Pencil.38828042.jpg"
+  },
+  {
+    "revision": "6c4f01d3be48a7463b561f39811d0fb2",
+    "url": "/react-portfolio-master.git/static/media/img.6c4f01d3.jpg"
+  },
+  {
+    "revision": "87561258a0fb17472008a4b2ff66696f",
+    "url": "/react-portfolio-master.git/static/media/laptop1.87561258.jpg"
+  }
+]);
